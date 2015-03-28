f04767b0ba9c4aa31a9c5dc39b55e6b444004acb
Trevor Meyer <meyer@272.com>
2015-03-28 12:29:37 -0400

    prototype project from Trevor

---
+++ Assets/InteractiveDemo/Scripts/lightScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightScript : MonoBehaviour 
+{
+	bool Impact = false;
+	public float Sqr;
+	// Use this for initialization
+	void Start () 
+	{
+		Impact = true;
+		gameObject.light.intensity = 7;
+		Sqr = gameObject.light.intensity * gameObject.light.intensity * (( gameObject.light.intensity < 0.0f ) ? -1.0f : 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Impact)
+		{
+			gameObject.light.intensity -= (1.0f / Time.deltaTime) * Sqr * .0001f;
+			if (gameObject.light.intensity <= 0)
+			{
+				Destroy (gameObject);
+			}
+		}
+	}
+}
