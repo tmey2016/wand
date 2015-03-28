295c9b8b76e7e183cc86ac379d6292c69c6e08f6
Trevor Meyer <meyer@272.com>
2015-03-28 18:34:05 -0400

    Game and Wand from Mahesh

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using Vuforia;
 using Prime31;
@@ -18,6 +19,9 @@ public class Game : MonoBehaviour {
 	private GUIStyle gs = null;
 	private GUIStyle gsText = null;
 
+    private Dictionary<string, int> PowerHash = new Dictionary<string,int>();
+    private Dictionary<string, int> VulnerabilityHash = new Dictionary<string,int>();
+
 	// Use this for initialization
 	void Start () {
 		//no auto-focus
@@ -33,14 +37,29 @@ public class Game : MonoBehaviour {
 		//start time
 		startTime = Time.time;
 
+
+        StartCoroutine(CalculateWinCondition(10.0f));
+        
 	}
 	
+    void UpdateSpellBook()
+    {
+        PowerHash["A"] = 6;
+        VulnerabilityHash["A"] = 6;
+
+        PowerHash["B"] = 4;
+        VulnerabilityHash["B"] = 2;
+
+        PowerHash["C"] = 2;
+        VulnerabilityHash["B"] = 1;
+    }
+
 	// Update is called once per frame
 	void Update () {
 		//time
 		if (Time.time - startTime >= 3) {
 			Wand wandComponent = wand.GetComponent<Wand>();
-			int quadrant = wandComponent.Quadrant();
+			//int quadrant = wandComponent.Quadrant();
 #if UNITY_IPHONE
 
 			lastMessageSent = System.Text.UTF8Encoding.UTF8.GetBytes(quadrant.ToString());
@@ -73,18 +92,18 @@ public class Game : MonoBehaviour {
 		
 
 		Wand wandComponent = wand.GetComponent<Wand>();
-		int quadrant = wandComponent.Quadrant();
+		//int quadrant = wandComponent.Quadrant();
 		//lastMessageReceived = System.BitConverter.GetBytes(4);
 
 		int buttonWidth = 140;
 		int buttonHeight = 50;
 
-		GUI.TextArea(new Rect(0, 0, buttonWidth, buttonHeight), "Me: " + quadrant.ToString (), gsText);
-		if (lastMessageReceived.GetLength (0) > 0) {
-			GUI.TextArea (new Rect(buttonWidth, 0, buttonWidth, buttonHeight), "You: " + System.Text.UTF8Encoding.UTF8.GetString( lastMessageReceived ), gsText); 
-		}
+        //GUI.TextArea(new Rect(0, 0, buttonWidth, buttonHeight), "Me: " + quadrant.ToString (), gsText);
+        //if (lastMessageReceived.GetLength (0) > 0) {
+        //    GUI.TextArea (new Rect(buttonWidth, 0, buttonWidth, buttonHeight), "You: " + System.Text.UTF8Encoding.UTF8.GetString( lastMessageReceived ), gsText); 
+        //}
 
-		GUI.TextArea (new Rect(buttonWidth * 2, 0, buttonWidth, buttonHeight), "Peers: " + MultiPeer.getConnectedPeers().Count, gsText); 
+		//GUI.TextArea (new Rect(buttonWidth * 2, 0, buttonWidth, buttonHeight), "Peers: " + MultiPeer.getConnectedPeers().Count, gsText); 
 		GUI.TextArea (new Rect(buttonWidth * 3, 0, buttonWidth, buttonHeight), "Sent: " + System.Text.UTF8Encoding.UTF8.GetString( lastMessageSent ), gsText); 
 
 		if (GUI.Button (new Rect(0, buttonHeight, buttonWidth, buttonHeight), "Focus", gs)) {
@@ -97,7 +116,7 @@ public class Game : MonoBehaviour {
 		}
 		if (GUI.Button (new Rect(buttonWidth*2, buttonHeight, buttonWidth, buttonHeight), "Invite", gs)) {
 			//GameKit get peer
-			MultiPeer.showPeerPicker();
+			//MultiPeer.showPeerPicker();
 		}
 		if (GUI.Button (new Rect(buttonWidth*3, buttonHeight, buttonWidth, buttonHeight), "Auto-Focus", gs)) {
 			continuousFocusState = !continuousFocusState;
@@ -108,4 +127,44 @@ public class Game : MonoBehaviour {
 			}
 		}
 	}
+
+    IEnumerator CalculateWinCondition(float Time)
+    {
+        Wand wandComponent = wand.GetComponent<Wand>(); 
+
+        string player1String = wandComponent.GetSpell();
+
+
+        
+        //Get PlayerString from the bluetooth data we received.
+        string player2String = "B";
+
+        yield return new WaitForSeconds(Time);
+
+        int player1Power = PowerHash[player1String];
+        int player2Power = PowerHash[player2String];
+        
+        int player1Vulnerability = VulnerabilityHash[player1String];
+        int player2Vulnerability = VulnerabilityHash[player2String];
+
+        var NetPower = player1Power - player2Power;
+        var NetVulnerability = player1Vulnerability - player2Vulnerability;
+        var NetScore = NetPower - NetVulnerability;
+        
+        if(NetScore > 0)
+        {
+            // Insert Win Screen here
+            Debug.Log("You Won the Duel");
+        }
+        else if(NetScore < 0)
+        {
+            // Insert Lose Screen here
+            Debug.Log("You Lost the Duel");
+        }
+        else
+        {
+            //Insert Draw Screen Here
+            Debug.Log("Draw");
+        }
+    }
 }
--- Assets/Wand.cs
+++ Assets/Wand.cs
@@ -1,7 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Wand : MonoBehaviour {
+
+    enum WandSpellState
+    {
+        StateInvalid,
+        StateA,
+        StateB,
+        StateC,
+        StateMax
+    }
+    
+    public Vector3 SpellA;
+    public Vector3 SpellB;
+    public Vector3 SpellC;
+
+    private WandSpellState currentSpell;
+
+    List<string> spellList = new List<string>();
+
+    private bool bStartDuel = false;
+
+    void Start()
+    {
+        currentSpell = WandSpellState.StateInvalid;
+
+        SpellA = new Vector3(0.5f, 1.0f, 0.0f);
+        SpellB = new Vector3(1.0f, 0.0f, 0.0f);
+        SpellC = new Vector3(0.0f, 0.0f, 0.0f);
+
+        spellList.Clear();
+
+        //InvokeRepeating("WaitForPrint", 0.0f, 10.0f);
+        StartRoundTimer(3);
+        
+
+    }
 
 	/**
 	 * Get number for the quadrant the wand is currently in
@@ -9,33 +45,107 @@ public class Wand : MonoBehaviour {
 	 * 1 2 
 	 * 3 4
 	 */
-	public int Quadrant() {
+	WandSpellState GetCurrentSpell() 
+    {
 		Vector3 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
-		if (viewportPosition.y >= 0.5f && viewportPosition.y <= 1.0f) {
-			if (viewportPosition.x < 0.5f && viewportPosition.x >= 0.0f) {
-				return 1;
-			} else if (viewportPosition.x >= 0.5f && viewportPosition.x <= 1.0f) {
-				return 2;
-			}
+        float minDistance = 10000000.0f;
+        viewportPosition.z = 0;
 
-		} else if (viewportPosition.y >= 0.0f && viewportPosition.y < 0.5f) {
-			if (viewportPosition.x < 0.5f && viewportPosition.x >= 0.0f) {
-				return 3;
-			} else if (viewportPosition.x >= 0.5f && viewportPosition.x <= 1.0f) {
-				return 4;
-			}
-		}
+        var distanceFromA = Vector3.Distance(viewportPosition, SpellA);
+        var distanceFromB = Vector3.Distance(viewportPosition, SpellC);
+        var distanceFromC = Vector3.Distance(viewportPosition, SpellB);
 
-		return 0;
+        if(distanceFromA < minDistance)
+        {
+            minDistance = distanceFromA;
+        }
+        
+        if(distanceFromB < minDistance)
+        {
+            minDistance = distanceFromB;
+        }
+        
+        if( distanceFromC < minDistance)
+        {
+            minDistance = distanceFromC;
+        }
+
+        if(minDistance == distanceFromA)
+        {
+            return WandSpellState.StateA;
+        }
+        else if(minDistance == distanceFromB)
+        {
+            return WandSpellState.StateB;
+        }
+        else if(minDistance == distanceFromC)
+        {
+            return WandSpellState.StateC;
+        }
+
+		return WandSpellState.StateInvalid;
 	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
-		Debug.Log (Quadrant());
+	void Update () 
+    {
+        if (!bStartDuel)
+            return; 
+
+        WandSpellState newSpell = GetCurrentSpell();
+
+        if(newSpell != currentSpell)
+        {
+            switch(newSpell)
+            {
+                case WandSpellState.StateA:
+                    spellList.Add("A");
+                    break;
+                
+                case WandSpellState.StateB:
+                    spellList.Add("B");
+                    break;
+                
+                case WandSpellState.StateC:
+                    spellList.Add("C");
+                    break;
+
+                default:
+                    Debug.Log("Invalid Spell!!!");
+                    break;
+            }
+
+            currentSpell = newSpell;
+            
+            if(spellList.Count > 5)
+            {
+                spellList.RemoveAt(0);
+            }
+
+            WaitForPrint();
+        }
 	}
+
+    void WaitForPrint()
+    {
+        Debug.Log(string.Join(string.Empty, spellList.ToArray()));
+    }
+
+    public string GetSpell()
+    {
+        return string.Join(string.Empty, spellList.ToArray());
+    }
+
+    IEnumerator StartRoundTimer(float startTime)
+    {
+        while( startTime > 0)
+        {
+            Debug.Log("Starting Game in " + startTime);
+            yield return new WaitForSeconds(1);
+            startTime--;
+        }
+
+        bStartDuel = true;
+        
+    }
 }
